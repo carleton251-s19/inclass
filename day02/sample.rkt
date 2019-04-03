1316b65973f12c4c3670aaec12f0358c07bfd6dd
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-04-03 14:52:36 -0500

    Scripted auto-commit on change (2019-04-03 14:52:36) by gitwatch.sh

--- day02/sample.rkt
+++ day02/sample.rkt
@@ -1,1 +1,14 @@
-;; sample code
+;; function that adds two numbers
+(define add-em
+  (lambda (x y)
+    (+ x y)))
+
+(
+ (lambda (x y)
+   (- x y))
+ 7 3)
+
+( (lambda (x y) (- x y))     7       3)
+(         -                  7       3)
+
+ 
