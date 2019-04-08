82aa46f7859758f3c3b6ce38e39f13affb6adc26
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-04-08 14:13:58 -0500

    Scripted auto-commit on change (2019-04-08 14:13:58) by gitwatch.sh

--- day04/samples.rkt
+++ day04/samples.rkt
@@ -5,3 +5,18 @@
         (cons start
             (lambda ()
               (gen-lazy-list (+ start 1) stop))))))
+
+; (subsets '(1 2 3))
+; ----> ( () (1) (2) (3) (1 2) (1 3) (2 3) (1 2 3))
+; but I don't care about order
+
+; (cons-each '1 '( (a b) (c) () (d e))
+; --->  ( (1 a b) (1 c) (1) (1 d e) )
+
+(define cons-each
+  (lambda (s lst)
+    .....))
+
+(define subsets
+  (lambda (lst)
+    ........))
