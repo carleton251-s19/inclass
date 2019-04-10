9c2e54b6255db03f1b6327adec1eed57e8e08409
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-04-10 14:16:43 -0500

    Scripted auto-commit on change (2019-04-10 14:16:43) by gitwatch.sh

--- day05/day05.rkt
+++ day05/day05.rkt
@@ -3,3 +3,25 @@
   (lambda (x)
     (+ x 1)))
 
+(addOne 5)
+
+(map addOne '(7 3 1 3 9))
+
+(foldl cons '() '(1 2 3 4))
+(foldr cons '() '(1 2 3 4))
+
+
+(define addTwoThings
+  (lambda (x y)
+    (+ x y)))
+
+(addTwoThings 3 5)
+
+(foldl addTwoThings 0 '(4 2 1 4 5))
+
+(define my-map...)
+(define my-foldl ...)
+(define my-foldr ...)
+
+
+
