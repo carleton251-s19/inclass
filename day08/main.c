15e10180b35ba5899fa529e09b47a9f6274ce47f
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-04-17 14:22:37 -0500

    Scripted auto-commit on change (2019-04-17 14:22:37) by gitwatch.sh

--- day08/main.c
+++ day08/main.c
@@ -3,5 +3,10 @@
 
 int main() {
     LinkedList *head = NULL;
-    
+    LinkedList *newHead =
+        insertFrontLL(head, 12);
+    newHead =
+        insertFrontLL(head, 15);
+    newHead =
+        insertFrontLL(head, 3);
 }
