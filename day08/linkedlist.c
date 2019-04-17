82ebbc222707ec27079d219f1e0342435d82f240
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-04-17 14:21:44 -0500

    Scripted auto-commit on change (2019-04-17 14:21:44) by gitwatch.sh

--- day08/linkedlist.c
+++ day08/linkedlist.c
@@ -4,3 +4,10 @@ struct LinkedList {
 };
 
 typedef struct LinkedList       LinkedList;
+
+LinkedList *insertFrontLL(LinkedList *list, int item) {
+    LinkedList *cell = malloc(sizeof(LinkedList));
+    (*cell).value = item;
+    (*cell).next = list;
+    return cell;
+}
