a9d748a310c597397dabb1340f466ce5d956c976
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-04-19 14:59:42 -0500

    Scripted auto-commit on change (2019-04-19 14:59:42) by gitwatch.sh

--- day08/stringstuff.c
+++ day08/stringstuff.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    char word[3];
+    char word[4];
     word[0] = 'c';
     word[1] = 'a';
     word[2] = 't';
+    word[3] = '\0';
 
     printf("%s\n",word);
 }
