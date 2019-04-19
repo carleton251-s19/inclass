320b6b231576abf3a7e8797e09b8717b8a1be473
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-04-19 15:19:18 -0500

    Scripted auto-commit on change (2019-04-19 15:19:18) by gitwatch.sh

--- day08/stringstuff.c
+++ day08/stringstuff.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *makeArrayOfSize10() {
+    // how do I know it is 10 long?
+    // I don't. I'm out of time. Fix me.
+    char *newWord = malloc(10*sizeof(int));
+    newWord[0] = 'e';
+    return newWord;
+}
+     
 int main() {
     char word[4];
     word[0] = 'c';
@@ -12,8 +20,11 @@ int main() {
 
     printf("%s\n", "dog");
 
-    char *fido = "dog";
-    printf("%s\n", fido);
-    fido[0] = 'h';
+    char *theNewArray = makeArrayOfSize10();
+    printf("%c\n",theNewArray[0]);
+    /* char *fido = "dog"; */
+    /* printf("%s\n", fido); */
+    /* fido[0] = 'h'; */
+    /* free(fido); */
     
 }
