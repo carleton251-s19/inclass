3b4ecf07ebd7e628a70211cf94ae4bd6e5a9a51c
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-05-22 14:42:16 -0500

    Scripted auto-commit on change (2019-05-22 14:42:16) by gitwatch.sh

--- day-scoping/Lambdas.java
+++ day-scoping/Lambdas.java
@@ -1,6 +1,5 @@
 interface Adam {
     public int f(int x, int y);
-    public int h(int x, int y);
 }
 
 public class Lambdas {
@@ -12,21 +11,21 @@ public class Lambdas {
     }
     
     public static void main(String[] args) {
-        // // Full inheritance glory
-        // Function add = new Function() {
-        //     public int f(int x, int y) {
-        //         return x + y;
-        //     }
-        // };
+        // Full inheritance glory
+        Function add = new Function() {
+            public int f(int x, int y) {
+                return x + y;
+            }
+        };
         
-        // evaluate(add, 5, 3);
+        evaluate(add, 5, 3);
 
-        // // Anonymous inner classes
-        // evaluate(new Function() {
-        //     public int f(int x, int y) {
-        //         return x - y;
-        //     }
-        // }, 5, 3);
+        // Anonymous inner classes
+        evaluate(new Function() {
+            public int f(int x, int y) {
+                return x - y;
+            }
+        }, 5, 3);
 
         // Java lambda expressions, much better
         Adam multiply = (x,y) -> x*y;
