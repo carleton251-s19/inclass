1d8c6361a4fe850499bd5b5da4e6d59dc38bc60d
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-05-29 13:55:39 -0500

    Scripted auto-commit on change (2019-05-29 13:55:39) by gitwatch.sh

---
+++ fptrs/fptr.c
@@ -0,0 +1,20 @@
+// How to pass functions as parameters in C
+
+#include <stdio.h>
+
+int add(int x, int y) {
+    return x + y;
+}
+
+int multiply(int x, int y) {
+    return x * y;
+}
+
+int doit(function f, int x, int y) {
+    return f(x,y);
+}
+
+int main() {
+    printf("%i\n",doit(add,3,5));
+    printf("%i\n",doit(multiply,3,5));
+}
