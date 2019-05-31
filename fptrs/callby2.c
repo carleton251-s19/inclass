da3edba01c9aad1995c5eeb659f4f83223d313b8
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-05-31 12:51:03 -0500

    Scripted auto-commit on change (2019-05-31 12:51:02) by gitwatch.sh

---
+++ fptrs/callby2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void foo(int *x) {
+   *x = 6;
+   x = malloc(sizeof(int));
+   *x = 7;
+}
+
+int main() {
+   int a = 5;
+   printf("%i\n",a);
+   foo(&a);
+   printf("%i\n",a);
+}
