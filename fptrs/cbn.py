119e5cbe0e3578eeb7de1ff97b8214b8a7e0806a
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-06-03 18:40:58 -0500

    Scripted auto-commit on change (2019-06-03 18:40:58) by gitwatch.sh

--- fptrs/cbn.py
+++ fptrs/cbn.py
@@ -1,11 +1,8 @@
-#include <stdio.h>
+def sum(index, low, high, expr):
+    total = 0
+    for index in range(low,high):
+        total = total + expr
+    return total
 
-void foo(int x) {
-   x = 6;
-}
-
-int main() {
-   int a = 5;
-   foo(a);
-   printf("%i\n",a);
-}
+x = [1,9,3,3]
+sum(i, 0, 4, x[i])
